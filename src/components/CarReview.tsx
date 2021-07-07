8f1f2221615d3d6107ccc8e2ed8c33770fc7ae7a
KowalewskiPawel <pawelkowalewsk@gmail.com>
2021-07-07 14:56:45 +0300

    car review page added

--- src/components/CarReview.tsx
+++ src/components/CarReview.tsx
@@ -39,17 +39,37 @@ function CarReview() {
         const singleReview = [...response.data.models[0].reviews].filter(
           (item) => item._id === id
         );
-        setReviewsList([...singleReview]);
+        return setReviewsList([...singleReview]);
       })
       .catch((err) => console.error(err));
   }, []);
 
   return (
-    <div className="main-container">
-      <img src={car} alt="small logo" className="logo-small" />
-      <h1 id="small-title">
+    <div className="review-container">
+      <img src={car} alt="small logo" />
+      <h1>
         {make} {model}
       </h1>
+      <div>
+        {reviewsList.map((item, index) => {
+          const tempDate = new Intl.DateTimeFormat("en-GB").format(
+            new Date(item.Date)
+          );
+          const date = tempDate.split("/").join("-");
+          return (
+            <div key={index}>
+              <p>Version: {item.Version}</p>
+              <p>Year: {item.Year}</p>
+              <p>Engine: {item.Engine}</p>
+              <p>General: {item.General}</p>
+              <p>Pros: {item.Pros}</p>
+              <p>Cons: {item.Cons}</p>
+              <p>Date: {date}</p>
+              <p>User: {item.User}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
